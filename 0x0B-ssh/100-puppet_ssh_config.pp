2ca06c3bae3e0855804854a9d66439d3ea35628c
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-12 17:45:15 +0200

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 #Puppet script that create ssh configuration file
+include stdlib
 
 file_line { 'Off the Password Authentication':
 	ensure => 'present',
-	path => '/etc/ssh/ssh_config',
-	line => 'PasswordAuthentication no',
+	path   => '/etc/ssh/ssh_config',
+	line   => 'PasswordAuthentication no',
 }
 
 file_line { 'file_location':
 	ensure =>'present',
-	path => 'etc/ssh/ssh_config',
-	line => 'IdentityFile ~/.ssh/school'
+	path   => 'etc/ssh/ssh_config',
+	line   => 'IdentityFile ~/.ssh/school'
 }
