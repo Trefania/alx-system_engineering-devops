d1acbafe0d50ae78314c76f1a2f548aa980fcf77
Trefania <107484364+Trefania@users.noreply.github.com>
2023-03-03 00:39:49 +0200

    APIs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,10 @@ import json
 
 def number_of_subscribers(subreddit):
     """
-    queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+    queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given,
+    the function should return 0.
     """
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,10 @@ import requests
 
 def recurse(subreddit, hot_list=[]):
     """
-    recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None.
+    recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit,
+    the function should return None.
     """
     url = 'https://www.reddit.com/r/{}/top.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
